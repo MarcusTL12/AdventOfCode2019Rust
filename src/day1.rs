8ef2a290461e9d3c8a15facb71d391ff3108a3ec
MarcusTL12 <marcuspaafjellet@gmail.com>
2019-12-28 02:36:34 +0100

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -2,14 +2,48 @@ use std::fs::File;
 use std::io::{BufReader, BufRead};
 
 
-pub fn part1() {
-    let inputfilename = "inputfiles/day1.txt";
+pub fn part1() -> std::io::Result<()> {
+    let file = File::open("inputfiles/day1.txt")?;
     
-    let file = File::open(inputfilename)
-        .expect("Could not open file.");
+    let ans: u32 = BufReader::new(file)
+        .lines()
+        .map(|x| x.expect("Line is fucked")
+            .parse()
+            .expect("NaNi")
+        )
+        .map(|x: u32| x / 3 - 2)
+        .sum();
     
-    for line in BufReader::new(file).lines() {
-        let line = line.expect("Line is fucked!");
-        println!("{}", line);
+    println!("Fuel: {}", ans);
+    
+    Ok(())
+}
+
+
+fn modfuel(mass: i32) -> i32 {
+    let fuel = mass / 3 - 2;
+    
+    if fuel <= 0 {
+        0
+    } else {
+        fuel + modfuel(fuel)
     }
+}
+
+
+pub fn part2() -> std::io::Result<()> {
+    let file = File::open("inputfiles/day1.txt")?;
+    
+    let ans: i32 = BufReader::new(file)
+        .lines()
+        .map(|x| x.expect("Line is fucked")
+            .parse()
+            .expect("NaNi")
+        )
+        .map(modfuel)
+        .sum();
+    
+    println!("Fuel: {}", ans);
+    
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
 mod day1;
 
 
-fn main() {
-    day1::part1();
+fn main() -> std::io::Result<()> {
+    let mut done = false;
+    
+    while !done {
+        let mut choice = String::new();
+        std::io::stdin().read_line(&mut choice).expect("Dafuq");
+        
+        let choice: Vec<u32> = choice.split_whitespace()
+            .map(|x| x.parse().expect("NaNi"))
+            .collect();
+        
+        if choice.len() == 2 {
+            match choice[0] {
+                0 => done = true,
+                1 => match choice[1] {
+                    1 => day1::part1()?,
+                    2 => day1::part2()?,
+                    _ => println!("Not implemented")
+                },
+                _ => println!("Not implemented")
+            }
+        } else {
+            println!("Only two parameters");
+        }
+    }
+    
+    Ok(())
 }
