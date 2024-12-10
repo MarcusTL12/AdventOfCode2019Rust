4ec34cdceba24966f508f0ede6896db5ff30d1c1
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-12-10 10:26:57 +0100

    Removed all old code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AdventOfCode2019Rust
 Doing adventofcode 2019 in rust. These solutions were not done during the event as i used Julia for that, but rather afterwards to learn rust.
+
+Edit: Redoing these for fun as I have learned much rust since I did these first.
--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,96 @@
-#![feature(int_roundings)]
+use std::{env, fmt::Display, fs::read_to_string, time::Instant};
 
-mod day1;
-mod day10;
-mod day11;
-mod day12;
-mod day13;
-mod day14;
-mod day15;
-mod day16;
-mod day17;
-mod day18;
-mod day19;
-mod day2;
-mod day20;
-mod day21;
-mod day22;
-mod day23;
-mod day24;
-mod day25;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
+use home::home_dir;
 
-mod intcode;
+// mod day1;
+
+enum TaskResult {
+    Number(i64),
+    Generic(Box<dyn Display>),
+}
+
+impl Display for TaskResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Number(n) => n.fmt(f),
+            Self::Generic(s) => s.fmt(f),
+        }
+    }
+}
+
+impl TaskResult {
+    fn generic<T: Display + 'static>(x: T) -> TaskResult {
+        Self::Generic(Box::new(x))
+    }
+}
+
+impl<T: TryInto<i64> + Clone + Display + 'static> From<T> for TaskResult
+where
+    T::Error: std::fmt::Debug,
+{
+    fn from(value: T) -> Self {
+        if let Ok(n) = value.clone().try_into() {
+            Self::Number(n)
+        } else {
+            Self::generic(value)
+        }
+    }
+}
+
+type Day = [fn(String) -> TaskResult; 2];
+
+const DAYS: &[Day] = &[
+    // day1::PARTS,
+];
+
+fn load_input(day: usize, example: usize) -> String {
+    let path = home_dir()
+        .unwrap()
+        .join("aoc-input/2024")
+        .join(format!("day{day}"))
+        .join(if example == 0 {
+            "input".to_string()
+        } else {
+            format!("ex{example}")
+        });
+
+    read_to_string(path).unwrap()
+}
 
 fn main() {
-    let funcs = [
-        day1::PARTS,
-        day2::PARTS,
-        day3::PARTS,
-        day4::PARTS,
-        day5::PARTS,
-        day6::PARTS,
-        day7::PARTS,
-        day8::PARTS,
-        day9::PARTS,
-        day10::PARTS,
-        day11::PARTS,
-        day12::PARTS,
-        day13::PARTS,
-        day14::PARTS,
-        day15::PARTS,
-        day16::PARTS,
-        day17::PARTS,
-        day18::PARTS,
-        day19::PARTS,
-        day20::PARTS,
-        day21::PARTS,
-        day22::PARTS,
-        day23::PARTS,
-        day24::PARTS,
-        day25::PARTS,
-    ];
-    let mut args = std::env::args();
-    args.next();
-    match args.next() {
-        Some(x) if x == "all" => {
-            println!("Running all days:");
-            println!("===========================");
-            let timer = std::time::Instant::now();
-            for (i, parts) in funcs.iter().enumerate() {
-                let subtimer = std::time::Instant::now();
-                println!("---------------------------");
-                println!("Running Day {}", i + 1);
-                println!("Part 1:");
-                parts[0]();
-                println!("{:?}\n", subtimer.elapsed());
+    let mut args = env::args();
 
-                let subtimer = std::time::Instant::now();
-                println!("Part 2:");
-                parts[1]();
-                println!("{:?}", subtimer.elapsed());
-            }
-            println!("===========================");
-            println!("Took {:?}", timer.elapsed());
-        }
-        Some(x) => {
-            if let Ok(x) = x.parse::<usize>() {
-                if let Some(y) = args.next() {
-                    if let Ok(y) = y.parse::<usize>() {
-                        if let Some(x) = funcs.get(x - 1) {
-                            if let Some(x) = x.get(y - 1) {
-                                let timer = std::time::Instant::now();
-                                x();
-                                println!("Took {:?}", timer.elapsed());
-                            } else {
-                                println!("Not implemented");
-                            }
-                        } else {
-                            println!("Not implemented");
-                        }
-                    } else {
-                        println!("Must enter numbers!");
-                    }
-                } else {
-                    println!("Pass day and part as commandline parameters");
-                }
-            } else {
-                println!("Must enter numbers!");
-            }
-        }
-        _ => println!(concat!(
-            "Run specific day with day and part as command ",
-            "line arguments, \nor run all days by giving \"all\" as argument"
-        )),
-    }
+    args.next();
+
+    let quest: usize = args
+        .next()
+        .expect("Give day number as first cli argument")
+        .parse()
+        .expect("Day number not numeric");
+
+    let part: usize = args
+        .next()
+        .expect("Give part as second cli argument")
+        .parse()
+        .expect("Part not numeric");
+
+    let example: usize = args.next().map(|s| s.parse().unwrap()).unwrap_or(0);
+
+    let t = Instant::now();
+
+    let input = load_input(quest, example);
+
+    let t_load = t.elapsed();
+
+    println!("Loading input took: {t_load:?}");
+
+    let t = Instant::now();
+
+    let result = DAYS[quest - 1][part - 1](input);
+
+    let t_solve = t.elapsed();
+
+    println!("{result}");
+
+    println!("Solving took: {t_solve:?}");
 }
