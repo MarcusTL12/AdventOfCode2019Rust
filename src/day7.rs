57692da5b6bd4d6e3ab987f6e33cde3f142910fa
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-12-12 16:40:34 +0100

    Day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -65,13 +65,50 @@ fn part1(input: String) -> TaskResult {
 fn part2(input: String) -> TaskResult {
     let program = parse_intcode_program(&input);
 
-    let (mut m, s, r) = IntcodeMachine::new_with_channel(program);
+    let mut max_val = 0;
 
-    s.send(0).unwrap();
-    s.send(4).unwrap();
-    m.run().unwrap();
+    let channels = (0..5)
+        .map(|_| channel::unbounded())
+        .collect::<ArrayVec<_, 5>>()
+        .into_inner()
+        .unwrap();
 
-    println!("{}", r.recv().unwrap());
+    let mut machines = Some(
+        (0..5)
+            .map(|i| {
+                IntcodeMachine::new(
+                    program.clone(),
+                    channels[i].1.clone(),
+                    channels[(i + 1) % 5].0.clone(),
+                )
+            })
+            .collect::<ArrayVec<_, 5>>()
+            .into_inner()
+            .unwrap(),
+    );
 
-    todo!()
+    for settings in permutations::<5>() {
+        for m in machines.as_mut().unwrap() {
+            m.load_progam(&program);
+        }
+
+        for (&x, (s, _)) in settings.iter().zip(&channels) {
+            s.send((x + 5) as i64).unwrap();
+        }
+
+        let handles = machines.take().unwrap().map(|mut m| {
+            thread::spawn(|| {
+                m.run().unwrap();
+                m
+            })
+        });
+
+        channels[0].0.send(0).unwrap();
+
+        machines = Some(handles.map(|handle| handle.join().unwrap()));
+
+        max_val = channels[0].1.recv().unwrap().max(max_val);
+    }
+
+    max_val.into()
 }
